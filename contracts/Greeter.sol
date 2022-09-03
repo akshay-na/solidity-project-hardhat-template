8c2f42b8c3c79e8f4cad72ac1dc2520edb57a3b9
akshay-na <67007337+akshay-na@users.noreply.github.com>
2022-09-03 19:24:35 +0530

    chore: testing precommti hook

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -4,17 +4,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Greeter {
-    string private greeting;
+    string public greeting;
 
     constructor(string memory _greeting) payable {
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
     }
 
+    // solhint-disable-next-line no-empty-blocks
     receive() external payable {}
 
     function greet() public view returns (string memory) {
         return greeting;
+
+
     }
 
     function setGreeting(string memory _greeting) public {
